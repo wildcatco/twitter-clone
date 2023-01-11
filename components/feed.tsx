076a0c1dadda8f239d77ecbe047f035ffd3b8a41
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-11 13:54:41 +0900

    Create input of feed section

--- components/feed.tsx
+++ components/feed.tsx
@@ -1,4 +1,5 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
+import Input from "./input";
 
 const Feed = () => {
   return (
@@ -9,6 +10,7 @@ const Feed = () => {
           <SparklesIcon className="h-5" />
         </div>
       </div>
+      <Input />
     </div>
   );
 };
